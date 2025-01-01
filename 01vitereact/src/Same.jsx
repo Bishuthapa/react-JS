041f8b38c105faaad26302957f4d67b3555d4d9a
Bishuthapa <thapabishu435@gmail.com>
2025-01-01 14:43:48 +0545

    initial commit

---
+++ 01vitereact/src/Same.jsx
@@ -0,0 +1,15 @@
+function Same() {
+    return(
+        <>
+        <div><h1 style={{ textAlign: "center" }}>Hello</h1>
+        <p> I&apos;m a Junior Software Engineer focusing on serverless architecture, 
+            android development, user experience, and product development.</p>
+        </div>
+        </>
+
+
+    )
+}
+
+
+export default Same;
