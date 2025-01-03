492833307e6e77b4ab0322cbf3dc81d4c4f1cb04
Bishuthapa <thapabishu435@gmail.com>
2025-01-03 23:19:12 +0545

    currency exchanger

---
+++ 06_CurrencyExchanger/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+import useCurrencyInfo from "../../05_passwordGenerator/src/hooks/useCurrencyInfo";
+function App(){
+  return(
+    <>
+    <div><h1>Currency Exchanger</h1></div>
+    </>
+  )
+}
+
+export default App;
