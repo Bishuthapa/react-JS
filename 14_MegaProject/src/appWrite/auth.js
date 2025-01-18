a9ebbcde3b3f03554e08aa4eea870c177581f2c8
Bishuthapa <thapabishu435@gmail.com>
2025-01-18 23:24:47 +0545

    initial commit

--- 14_MegaProject/src/appWrite/auth.js
+++ 14_MegaProject/src/appWrite/auth.js
@@ -1,3 +1,43 @@
+import config from "./config";
 import {Client, Account , ID} from "appwrite";
+
+export class AuthService {
+    Client = new Client();
+    account;
+
+    constructor() {
+        this.Client
+            .setEndpoint(config.appwriteEndpoint)
+            .setProject(config.appwriteProjectId)
+            this.account = new Account(this.Client);
+    }
+
+    async CreateAccount({email,password,name}) {
+        
+
+            const userAccount = await this.account.create( ID.unique, email, password, name)
+
+            if(userAccount) {
+                return this.Login({email,password});
+
+
+            }
+            else{
+                return userAccount;
+            }
+            
+        
+        
+    }
+
+    async Login({email,password}) {
+        return await this.account.createEmailSession(email,password);
+    }
+}
+
+
+const authService = new AuthService();
+
+export default authService
 
  
