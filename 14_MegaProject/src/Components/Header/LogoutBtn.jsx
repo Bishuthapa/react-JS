ee0f2743ecebfbdefc1af20a17d0387d6d643f2c
Bishuthapa <thapabishu435@gmail.com>
2025-01-22 23:49:36 +0545

    react

--- 14_MegaProject/src/Components/Header/LogoutBtn.jsx
+++ 14_MegaProject/src/Components/Header/LogoutBtn.jsx
@@ -1,11 +1,21 @@
 import { useDispatch } from "react-redux";
 import { logout } from "../../Store/authSlice";
 import authService from "../../appwrite/auth";
-function LogoutBthn() {
-    const dispatch = useDispatch();
+function LogoutBtn() {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    })
+  }
+
+
   return (
-    <div> LogoutBthn </div>
-  );
+    <button
+    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}
+    >Logout</button>
+  )
 }
 
-export default LogoutBthn
+export default LogoutBtn;
